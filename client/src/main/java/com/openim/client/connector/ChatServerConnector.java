0ae3b550099f54b3f16eba2da35090a115e88da0
shihuacai <shihuacai1989@126.com>
2015-08-06 21:32:38 +0800

    初次提交

---
+++ client/src/main/java/com/openim/client/connector/ChatServerConnector.java
@@ -0,0 +1,11 @@
+package com.openim.client.connector;
+
+/**
+ * Created by shihuacai on 2015/8/6.
+ */
+public class ChatServerConnector implements IConnector {
+    @Override
+    public boolean connect() {
+        return false;
+    }
+}
---
+++ client/src/main/java/com/openim/client/connector/IConnector.java
@@ -0,0 +1,8 @@
+package com.openim.client.connector;
+
+/**
+ * Created by shihuacai on 2015/8/6.
+ */
+public interface IConnector {
+    public boolean connect();
+}
